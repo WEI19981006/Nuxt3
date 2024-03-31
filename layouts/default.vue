891fb6b29cb67275e8edf8411d0959ee1abb3db3
weicoding1006@gmail.com <weicoding1006@gmail.com>
2024-03-31 12:41:20 +0800

    初版

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="">
+        <header class="w-full bg-slate-400 py-2 flex items-center justify-between px-5">
+            <h1 class=" text-3xl">Logo</h1>
+            <nav class=" flex gap-2">
+                <NuxtLink to="/" class="block px-3 py-2  box-border" :class="{ ' border-b-2': isActive('/') }">首頁
+                </NuxtLink>
+                <NuxtLink to="/about" class=" block px-3 py-2" :class="{ 'border-b-2': isActive('/about') }">About
+                </NuxtLink>
+            </nav>
+        </header>
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+import { useRoute } from "vue-router";
+const route = useRoute();
+function isActive(path) {
+    return route.path === path;
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container mx-auto">
+        <h1>首頁</h1>
+        <p>高度:{{ size.height }}</p>
+        <p>寬度:{{size.width}}</p>
+
+
+    </div>
+</template>
+
+<script setup>
+    import {useWindowSize} from "@vueuse/core"
+    const size = useWindowSize();
+    onMounted(() => {
+        console.log(size);
+    });
+</script>
+
+<style scoped>
+
+</style>
